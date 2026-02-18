a5a5895f0b662fa663ad5ec14d1355ad329d14e2
Himanshu4234 <himanshuchauhan85.hc@gmail.com>
2026-02-18 18:01:13 +0530

    final issue solve

--- src/resources/once-ui.config.ts
+++ src/resources/once-ui.config.ts
@@ -14,7 +14,7 @@ import {
 import { home } from "./index";
 
 // IMPORTANT: Replace with your own domain address - it's used for SEO in meta tags and schema
-const baseURL: string = "https://prashant-singh-portfolio.vercel.app";
+const baseURL: string = "https://himanshusinghchauhanportfolio.netlify.app";
 
 const routes: RoutesConfig = {
   "/": true,
